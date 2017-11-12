392e35b88570a628acc7d0d8003105d8b71baade
Lisa <lisafolkerson@gmail.com>
2017-11-12 18:17:07 -0500

    organizers and code of conduct pages

---
+++ payment_CAD.php
@@ -0,0 +1,33 @@
+
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" >
+<input type="hidden" name="cmd" value="_cart">
+<input type="hidden" name="business" value="KZPB2FMKTFX46">
+<input type="hidden" name="lc" value="CA">
+<input type="hidden" name="item_name" value="#a11yTO Camp Sponsorship">
+<input type="hidden" name="item_number" value="A11YTO">
+<input type="hidden" name="currency_code" value="CAD">
+<input type="hidden" name="button_subtype" value="products">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="cn" value="Add special instructions to the seller:">
+<input type="hidden" name="no_shipping" value="2">
+<input type="hidden" name="return" value="http://www.accessibilitycampto.org/payment_thankyou.php">
+<input type="hidden" name="cancel_return" value="http://www.accessibilitycampto.org/">
+<input type="hidden" name="add" value="1">
+<input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHosted">
+<table>
+<tr>
+	<td><input type="hidden" name="on0" value="Sponsor Name">Sponsor Name</td>
+	<td><input type="text" name="os0" maxlength="200"></td>
+	</tr>
+<tr>
+	<td>Amount</td>
+	<td><input type="tel" name="amount" maxlength="50"></td>
+	</tr>
+
+
+
+
+</table>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
